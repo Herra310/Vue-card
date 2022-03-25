85d9b5c6758e0a1b137a68d4dce6c236708f5f06
Herra310 <jorgehr99jhr@gmail.com>
2022-03-24 19:08:54 -0700

    Componente terminado

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,109 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main id="app" @mousemove="mousemove">
+    <section class="products">
+      <Product
+        v-for="product in products"
+        :key="product.color"
+        :product="product"
+      />
+    </section>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Product from "./components/Product";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Product,
+  },
+  data() {
+    return {
+      products: [
+        {
+          title: "Nike Air Max",
+          color: "green",
+          bgtext: "NIKE",
+          src: require("./assets/green-shoe.png"),
+        },
+        {
+          title: "Nike flex",
+          color: "blue",
+          bgtext: "AIR",
+          src: require("./assets/blue-shoe.png"),
+        },
+        {
+          title: "Nike Roche Runs",
+          color: "pink",
+          bgtext: "MAX",
+          src: require("./assets/pink-shoe.png"),
+        },
+      ],
+    };
+  },
+  methods: {
+    mousemove(e) {
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let products = document.querySelectorAll(".products .product");
+
+      for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+
+        let productimg = product.querySelector(".product-img-wrap");
+
+        let imgX = mouseX - this.coords(productimg).x;
+        let imgY = mouseY - this.coords(productimg).y;
+
+        productimg.style.transform = `translateY(-${imgY/35}px) translateX(-${imgX/35}px) 
+        translateZ(120px)`;
+
+        let bgtext = product.querySelector('.bg-text');
+        let bgX = mouseX - this.coords(bgtext).x;
+                let bgY = mouseY - this.coords(bgtext).y;
+
+        bgtext.style.transform = ` translateX(${bgX/25}px) translateY(${bgY/25}px)`;
+      }
+    },
+    coords(el){
+      let coords = el.getBoundingClientRect();
+
+      return {
+        x: coords.left /2,
+        y: coords.top /2,
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif;
+}
+
+main {
+  width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+
+  background-color: #eee;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.products {
+  display: flex;
+  max-width: 1200px;
+  padding: 25px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Product.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="product">
+    <div :class="'product-inner ' + product.color">
+      <div class="product-text-wrap">
+        <h2 class="bg-text">
+          {{ product.bgtext }}
+        </h2>
+      </div>
+      <div class="product-img-wrap">
+        <img :src="product.src" class="imgs" />
+      </div>
+      <div class="product-detail">
+        <h2>{{ product.title }}</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+          amet culpa aperiam reiciendis quis veniam hic officia rem id sunt
+          asperiores sit dolore modi libero fugit autem recusandae, ducimus
+          doloremque!
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "product",
+  props: ["product"],
+};
+</script>
+
+<style>
+.product {
+  flex: 1 1 33.333%;
+  width: 100%;
+  padding: 25px;
+}
+
+.product-inner {
+  position: relative;
+  padding: 25px;
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.25);
+  perspective: 1100px;
+}
+
+.product-inner.green {
+  background-image: linear-gradient(to bottom right, #24d484, #116432);
+}
+.product-inner.blue {
+  background-image: linear-gradient(to bottom left, #24d484, #2474c4 70%);
+}
+.product-inner.pink {
+  background-image: linear-gradient(to bottom right, #f444a4, #1168d4);
+}
+
+.product-text-wrap {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 0;
+  overflow: hidden;
+  perspective: 1100px;
+}
+
+.product-text-wrap h2 {
+  font-size: 120px;
+  color: #313131;
+  font-weight: 900;
+  opacity: 0.2;
+  transform-origin: center;
+}
+
+.product-img-wrap {
+  position: relative;
+  z-index: 1;
+  transform-origin: center;
+}
+
+.product-img-wrap .imgs {
+  width: 100%;
+  filter: drop-shadow(0px 0px 12px rgba(0, 0, 0, 0.25));
+}
+
+.product-detail {
+  background: #eee;
+  padding: 25px;
+  margin: 0px -25px -25px;
+}
+
+.product-detail h2 {
+  font-size: 24px;
+  font-weight: 700px;
+  color: #676767;
+  margin-bottom: 15px;
+}
+
+.product-detail p {
+  font-size: 14px;
+  line-height: 1.5;
+  font-weight: 300;
+  color: #676767;
+}
+</style>
